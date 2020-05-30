8c625664dad23bc7994df8400ce19da7d1779273
Agent6-6-6 <36808164+Agent6-6-6@users.noreply.github.com>
2020-05-30 21:08:34 +1200

    Update to use Excel to export, removed oletools method - closes #3

--- README.md
+++ README.md
@@ -6,27 +6,27 @@
 # Excel VBA & XML Export Pre-Commit Hook
 
 ### What do these Pre-commit scripts do?
-When you commit an Excel file, these scripts extract all VBA normal modules, class modules & forms, and the customUI.xml & customUI14.xml files. 
+When you commit an Excel file, these scripts extract all VBA normal modules, class modules & forms, and the customUI.xml & customUI14.xml files.
 
 ### Why did I create these scripts
-Basically I was looking for a way to automate a way of using the version control offered by Git/Github with Excel files and the VBA code modules stored within an Excel file. 
+Basically I was looking for a way to automate a way of using the version control offered by Git/Github with Excel files and the VBA code modules stored within an Excel file.
 
 I'm a heavy user of VBA in engineering templates and often tweak code in individual templates that must then be transferred to other templates. Exporting VBA modules by hand or copying sections of code within the VBE is pretty cumbersome, extracting the xml files from the excel archives by hand is even more cumbersome.
 
-The only thing I could find using good old Google was this one page at [xltrail](https://www.xltrail.com/blog/auto-export-vba-commit-hook), which sort of did at a high level what I was after. So with the ideas given in the code provided and some tweaking, I set about creating something that worked for my particular workflow. 
+The only thing I could find using good old Google was this one page at [xltrail](https://www.xltrail.com/blog/auto-export-vba-commit-hook), which sort of did at a high level what I was after. So with the ideas given in the code provided and some tweaking, I set about creating something that worked for my particular workflow.
 
 ### How to use these scripts:-
 Basically put the three pre-commit files within the `.git\hooks` directory of a repository.
 
-Save/copy any Excel file (or multiple Excel files) into the repositories root directory, commit any changes to tracked files and the scripts take care of extracting the VBA (forms/modules/class modules) and customUI XML files into *.XML & *.VBA subdirectories and adds the extracted VBA & XML files to the commit. 
+Save/copy any Excel file (or multiple Excel files) into the repositories root directory, commit any changes to tracked files and the scripts take care of extracting the VBA (forms/modules/class modules) and customUI XML files into *.XML & *.VBA subdirectories and adds the extracted VBA & XML files to the commit.
 
-This effectively adds version control for VBA modules and the customUI.xml files (Ribbon) within Excel spreadsheets or add-ins. If no VBA or customUI is present, then no subdirectories are created for that particular component. Every subsequent change to the Excel file is picked up by git once you save as a change. When you commit these changes, the pre-commit hook runs the scripts again to extract the VBA & XML, then they are added/removed from the commit, rinse and repeat to infinity. If you delete or rename an excel file the associated subdirectories are also removed. 
+This effectively adds version control for VBA modules and the customUI.xml files (Ribbon) within Excel spreadsheets or add-ins. If no VBA or customUI is present, then no subdirectories are created for that particular component. Every subsequent change to the Excel file is picked up by git once you save as a change. When you commit these changes, the pre-commit hook runs the scripts again to extract the VBA & XML, then they are added/removed from the commit, rinse and repeat to infinity. If you delete or rename an excel file the associated subdirectories are also removed.
 
 If you want every new repository to use the pre-commit files, copy them into the `\Program Files\Git\mingw64\share\git-core\templates\hooks` directory in Windows.
 
 Some Excel test files with varying VBA and XML content are provided to test the functionality if you clone the repository, simply unzip the files and do a commit to see what is going on.
 
-My first time using python so if it doesn't work for you, I'm probably not going to be of much help. But if its any consolation its exactly what I was after for my own workflow, want it to do something different then adapt it to your own needs.
+If you want it to do something different then adapt it to your own needs.
 
 ### A bit about my Excel workflow
 The included .gitignore file is setup to only commit files within the root directory of the repository, thats how I roll with Excel.
@@ -40,9 +40,11 @@ The addin/templates contains a few custom ribbon tabs that contain buttons for e
 If I am making small changes I'll just do it in a master branch and commit to github to update the code modules stored on github, if doing major changes I'll do a branch until things are finalised. You just need to keep in mind because of the binary nature of the Excel files, you cannot work in two branches as there is no way to merge the code from two competing branches into the excel file itself. This pre-commit only extracts the code as its stored within a file. If changes are made in both branches then you need to reconcile them by hand before committing/merging one of the branches.
 
 ### Prerequisites
-python 3.x (My first time using python so no idea if it will work under python 2.x, test it and let me know)
+Python 3.x (Tested as working with Python 3.8.x)
 
-oletools (for extracting the VBA)
+Excel (Tested with Excel/Microsoft 365. Excel is used for exporting the VBA modules)
+
+An earlier version of this tool used/required oletools for extracting the VBA. However this was unable to extract both files for userforms.
 
 To get the VBA script working you'll need to enable programmatic access to the VBA project within Excel. You can do this by going -> File -> Options -> Trust Center -> Trust Center Settings -> Macro Settings -> activate checkmark on `Trust access to the VBA project object model`
 
--- pre-commit-VBA.py
+++ pre-commit-VBA.py
@@ -1,7 +1,7 @@
 '''
 pre-commit-VBA.py
 
-Last updated 23/02/2019
+Last updated 30/05/2020
 
 Script extracts VBA modules, forms and class modules into a new 'FILENAME.VBA' subdirectory within the repository root folder (by default)
 
@@ -10,23 +10,57 @@ With the standard .gitignore entries, only Excel files located within the root d
 
 import os
 import shutil
+import win32com.client
 
-from oletools.olevba3 import VBA_Parser
 
-# list excel extensions that will be processed and have VBA modules extracted
-excel_file_extensions = ('xlsb', 'xls', 'xlsx', 'xlsm', 'xla', 'xlt', 'xlam', 'xltm')
+def init_Xl(file_path):
+    '''function to open Excel and open Workbook at file_path
 
-# Set this to 'True' if you would like to retain the file headers in CLASS (.cls) and FORM (.frm) modules
-keep_header = False
+    :param file_path: full path of Workbook to open/process
+    :type file_path: str
+    :return: Xl_app ('Microsoft Excel'), Excel Workbook (<COMObject Open>)
+    :rtype: str
+    '''
+    # open Excel as hidden instance in the background
+    Xl_app = win32com.client.DispatchEx('Excel.Application')
+    # required to disable all macros in all files opened programmatically without showing any security alerts. Equivalent of Application.AutomationSecurity = msoAutomationSecurityForceDisable
+    Xl_app.AutomationSecurity = 3
+    # required to suppress the overwriting of existing file
+    Xl_app.DisplayAlerts = False
+    # required to suppress any application events from running on opening excel files
+    Xl_app.EnableEvents = False
+    # open Workbook
+    Xl_wb = Xl_app.Workbooks.Open(Filename=file_path, Editable=True)
+    print('Starting Excel instance...')
+    return Xl_app, Xl_wb
 
-# process Excel files in this directory (not recursive to subdirectories)
-directory = '.'
 
-# String to append to end of subdirectory
-VBA_suffix = '.VBA'
+def close_Xl(Xl_app, Xl_wb):  # , file_path, Xl_file_format):
+    '''function to save & close Workbook instance
 
-# String to append to end of subdirectory based on filename
-Rev_tag = ' - Rev '
+    :param Xl_app: 'Microsoft Excel'
+    :type Xl_app: str
+    :param Xl_wb: Microsoft Excel Workbook COMObject
+    :type Xl_wb: str
+    '''
+    print('Closing Excel instance...')
+    Xl_app.Workbooks(Xl_wb.Name).Close(SaveChanges=False)
+
+
+def module_file_ext(Xl_module_type):
+    '''function to return the standard extensions based on module type numeric reference
+
+    :param Xl_module_type: Module type codes
+        * Type = 1   = Standard Module    -- (*.bas)
+        * Type = 2   = Class Module       -- (*.cls)
+        * Type = 3   = Userform Module    -- (*.frm)
+        * Type = 100 = Sheet Class Module -- (*.cls)
+    :type Xl_module_type: int
+    :return: standard file extension based on module type
+    :rtype: str
+    '''
+    module_type_dict = {1: '.bas', 2: '.cls', 3: '.frm', 100: '.cls'}
+    return module_type_dict[Xl_module_type]
 
 
 # function to extract the VBA modules from an Excel archive
@@ -42,7 +76,7 @@ def extract_VBA_files(workbook_name):
         filename = os.path.splitext(workbook_name)[0]
 
     # Setup directory name based on filename
-    vba_path = filename + VBA_suffix
+    vba_path = directory + filename + VBA_suffix
 
     # Make new directory (to replace existing where it previously existed and was removed)
     os.mkdir(vba_path)
@@ -55,36 +89,51 @@ def extract_VBA_files(workbook_name):
     print('Directory name -- ' + vba_path)
     print('-' * max_len)
 
-    # setup for removal of VBA modules
-    vba_parser = VBA_Parser(workbook_name)
-    vba_modules = vba_parser.extract_all_macros() if vba_parser.detect_vba_macros() else []
+    # Open Excel instance and open workbook
+    Xl_app, Xl_wb = init_Xl(directory + workbook_name)
 
-    # extract VBA modules
-    for _, _, module_name, content in vba_modules:
-        decoded_content = content.decode('latin-1')
-        lines = []
-        if '\r\n' in decoded_content:
-            lines = decoded_content.split('\r\n')
-        else:
-            lines = decoded_content.split('\n')
-        if lines:
-            # Modules can have a variable number of non-code lines at the beginning,
-            # these headers can be left or removed based on the keep_header variable.
-            # All headers begin with 'Attribute VB_' string
-            content = lines if keep_header else lines[decoded_content.count('Attribute VB_') :]
+    # Iterate through each VBComponent (Sheet Class Modules, Class Modules, Standard Modules, Userforms)
+    try:
+        for Xl_module in Xl_wb.VBProject.VBComponents:
+            # get name of module
+            Xl_module_name = Xl_module.Name
+            # get module type
+            Xl_module_type = Xl_module.Type
 
-            if content and content[-1] == '':
-                content.pop(len(content) - 1)
-                # check for empty modules (not processed if empty, modules won't be empty if headers are kept even if there is no content)
-                non_empty_lines_of_code = len([c for c in content if c])
+            module_name = f'{Xl_module_name}{module_file_ext(Xl_module_type)}'  # filename e.g. 'module.bas'
+            module_path = f'{vba_path}\\{module_name}'
 
-                if non_empty_lines_of_code > 0:
+            # for standard modules, class modules and userforms
+            if Xl_module_type in [1, 2, 3]:
+                # export module
+                Xl_wb.VBProject.VBComponents(Xl_module_name).Export(module_path)
+                print(f'Exporting -- {module_name}')
 
-                    with open(os.path.join(vba_path, module_name), 'w', encoding='utf-8') as module_file:
-                        module_file.write('\n'.join(content))
-                        # print to console for information/debugging purposes
-                        print('Extracted -- ' + module_name + ' from ' + workbook_name)
+            # for sheet class modules
+            elif Xl_module_type in [100]:
+                # export module
+                Xl_wb.VBProject.VBComponents(Xl_module_name).Export(module_path)
+                print(f'Exporting -- {module_name}')
 
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        # close Excel instance
+        close_Xl(Xl_app, Xl_wb)
+
+
+# list excel extensions that will be processed and have VBA modules extracted
+excel_file_extensions = ('xlsb', 'xls', 'xlsx', 'xlsm', 'xla', 'xlt', 'xlam', 'xltm')
+
+# process Excel files in root directory (not recursive to subdirectories)
+directory = os.getcwd() + '//'
+
+# String to append to end of subdirectory
+VBA_suffix = '.VBA'
+
+# String to remove from end of filename
+Rev_tag = ' - Rev '
 
 # remove all previous '.VBA' directories including contents
 for directories in os.listdir(directory):
@@ -96,12 +145,13 @@ for filenames in os.listdir(directory):
     if filenames.endswith(excel_file_extensions):
         # skip temporary excel files ~$*.*, otherwise process Excel file and extract VBA modules
         if not filenames.startswith('~$'):
+            # extract VBA modules
             extract_VBA_files(filenames)
 
-# print trailing line to separate output
+# print trailing line to separate output from multiple files
 print()
 
-# loop through directories & remove '.VBA' directory if nothing was extracted and it is therefore empty
+# loop through directories & remove '.VBA' directory if nothing was extracted and directory is empty
 for directories in os.listdir(directory):
     if directories.endswith(VBA_suffix):
         if not os.listdir(directories):
